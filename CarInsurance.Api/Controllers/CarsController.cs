e721f1ef9712390d9e1459ff2986ca9a6eadd399
Diana <diana-maria.chelea@student.tuiasi.ro>
2025-08-30 15:25:19 +0300

    task3 (2)

--- CarInsurance.Api/Controllers/CarsController.cs
+++ CarInsurance.Api/Controllers/CarsController.cs
@@ -2,6 +2,7 @@ using CarInsurance.Api.Dtos;
 using CarInsurance.Api.Models;
 using CarInsurance.Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace CarInsurance.Api.Controllers;
 
@@ -18,8 +19,12 @@ public class CarsController(CarService service) : ControllerBase
     [HttpGet("cars/{carId:long}/insurance-valid")]
     public async Task<ActionResult<InsuranceValidityResponse>> IsInsuranceValid(long carId, [FromQuery] string date)
     {
-        if (!DateOnly.TryParse(date, out var parsed))
-            return BadRequest("Invalid date format. Use YYYY-MM-DD.");
+        if (string.IsNullOrWhiteSpace(date))
+            return BadRequest("Write a date. Format: YYYY-MM-DD.");
+
+        if (!DateOnly.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out var parsed))
+            return BadRequest("Invalid date or date format(YYYY-MM-DD).");
 
         try
         {
